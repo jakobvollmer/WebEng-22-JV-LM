8580fad8258b15f7994f2359064be21b0aa28333
Jakob Vollmer <jakob@vollmer.email>
2022-12-01 00:53:19 +0100

    logging added

--- app/run.py
+++ app/run.py
@@ -1,8 +1,22 @@
+import logging, os
+from const import DEFAULTS
+from distutils.util import strtobool
+
+logLevel:str = os.getenv("LOG_LEVEL", DEFAULTS.LOG_LEVEL).upper()
+logFormat:str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logToConsole:bool = bool(strtobool(os.getenv("LOG_TO_CONSOLE", DEFAULTS.LOG_TO_CONSOLE)))
+
+if logToConsole:
+    logging.basicConfig(format=logFormat, level=logLevel)
+else:
+    logging.basicConfig(filename="./log/app.log", format=logFormat, level=logLevel)
+    
+log = logging.getLogger()
+log.info("Init app...")
+
 from flask import Flask
 
-from api.auth.auth import validateAuth
 from api.errorHandler import errorHandler
-
 from api.reservations.getAll import getReservationsAll
 from api.reservations.getById import getReservationsById
 
@@ -10,9 +24,7 @@ from db.postqresDB import get_PostqresDB
 db = get_PostqresDB()
 
 app = Flask(__name__)
-
 app.register_blueprint(errorHandler)
 app.register_blueprint(getReservationsAll)
 app.register_blueprint(getReservationsById)
-
-app.run(host="0.0.0.0", port=9000)
+app.run(host="0.0.0.0", port=os.getenv("RESERVATIONS_APP_PORT", DEFAULTS.RESERVATIONS_APP_PORT))
