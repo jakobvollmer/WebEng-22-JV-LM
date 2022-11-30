35bb19db645d5c91f10c6c747554c72431e03b9c
Jakob Vollmer <jakob@vollmer.email>
2022-12-01 00:51:12 +0100

    Add internalServerError handler

--- app/api/errorHandler.py
+++ app/api/errorHandler.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # encoding: utf-8
 
-from flask import Blueprint
+from flask import Blueprint, request
 import logging
 
 from api import errors, responses
@@ -9,10 +9,17 @@ from api import errors, responses
 errorHandler = Blueprint("errorHandler", __name__)
 log = logging.getLogger("errorHandler")
 
-# JsonContentCorrupt
+# Unauthorized
 @errorHandler.app_errorhandler(errors.Unauthorized)
 def handle_unauthorized(e):
-    log.warning(e)
+    log.warning("Exception Unauthorized raised at request " + request.base_url + " . Exception: " + str(e))
     return responses.UNAUTHORIZED("Authorization missing or failed.")
 
+# InternalServerError
+@errorHandler.app_errorhandler(errors.InternalServerError)
+def handle_internal_server_error(e):
+    log.warning("Exception InternalServerError raised. Exception: " + str(e))
+    return responses.InternalServerError()
+
 errorHandler.register_error_handler(401, handle_unauthorized)
+errorHandler.register_error_handler(500, handle_internal_server_error)
