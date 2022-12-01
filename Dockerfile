f36ed43d302bfd56d96536fab68668f01e91b47e
Jakob Vollmer <jakob@vollmer.email>
2022-12-01 01:33:36 +0100

    Export log file from container implemented

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
 FROM python:3.9
 
-WORKDIR /var/www/
+RUN mkdir -p /log/
 
+WORKDIR /var/www/
 COPY ./requirements.txt ./
 RUN pip3 install -r ./requirements.txt
 
 COPY ./app ./app
-WORKDIR /var/www/app
 
+WORKDIR /var/www/app
 CMD [ "python3", "run.py"]
--- app/run.py
+++ app/run.py
@@ -1,19 +1,6 @@
 import logging, os
 from const import DEFAULTS
 from distutils.util import strtobool
-
-logLevel:str = os.getenv("LOG_LEVEL", DEFAULTS.LOG_LEVEL).upper()
-logFormat:str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-logToConsole:bool = bool(strtobool(os.getenv("LOG_TO_CONSOLE", DEFAULTS.LOG_TO_CONSOLE)))
-
-if logToConsole:
-    logging.basicConfig(format=logFormat, level=logLevel)
-else:
-    logging.basicConfig(filename="./log/app.log", format=logFormat, level=logLevel)
-    
-log = logging.getLogger()
-log.info("Init app...")
-
 from flask import Flask
 
 from api.errorHandler import errorHandler
@@ -21,6 +8,21 @@ from api.reservations.getAll import getReservationsAll
 from api.reservations.getById import getReservationsById
 
 from db.postqresDB import get_PostqresDB
+
+logLevel:str = os.getenv("LOG_LEVEL", DEFAULTS.LOG_LEVEL).upper()
+logFormat:str = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logToConsole:bool = bool(strtobool(os.getenv("LOG_TO_CONSOLE", DEFAULTS.LOG_TO_CONSOLE)))
+encoding="utf-8"
+
+if logToConsole:
+    logging.basicConfig(format=logFormat, encoding=encoding, level=logLevel)
+else:
+    logging.basicConfig(filename="/log/app.log", encoding=encoding, format=logFormat, level=logLevel)
+logging.getLogger("werkzeug").setLevel(logLevel)
+
+log = logging.getLogger()
+log.info("Init app...")
+
 db = get_PostqresDB()
 
 app = Flask(__name__)
