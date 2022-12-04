3f8182033a2be5f745b69437ba4c0eca2724c733
jakobvollmer <jovo4284@gmail.com>
2022-12-04 20:48:11 +0000

    Aktualisieren test/copyCompose.py

--- test/copyCompose.py
+++ test/copyCompose.py
@@ -3,12 +3,12 @@ from yaml.loader import SafeLoader
 
 compose:str = ""
 composeBackend:str = ""
-with open("./compose/compose.yml") as f:
+with open("compose/compose.yml") as f:
     compose = yaml.load(f, Loader=SafeLoader)
 
-with open("./compose.yml") as f:
+with open("compose.yml") as f:
     composeBackend = yaml.load(f, Loader=SafeLoader)
 
 compose["services"]["backend-reservations"] = composeBackend["services"]["backend-reservations"]
-with open("./compose/compose.yml", 'w') as f:
+with open("compose/compose.yml", 'w') as f:
     data = yaml.dump(compose, f)
