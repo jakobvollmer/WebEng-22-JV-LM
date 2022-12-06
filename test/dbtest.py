176f0e1a0a2925241f13cfa2859f76a50edd78fd
Jakob Vollmer <jakob@vollmer.email>
2022-12-06 09:13:44 +0100

    Test update

--- test/dbtest.py
+++ test/dbtest.py
@@ -3,11 +3,11 @@ import requests, unittest, json
 
 class Testing(unittest.TestCase):
     def test_get_status_statuscode (self):
-        result = requests.get("http://127.0.0.1/reservations/status/")
+        result = requests.get("http://0.0.0.0/reservations/status/")
         self.assertEqual(result.status_code, 200)
 
     def test_get_status_body (self):
-        result = requests.get("http://127.0.0.1/reservations/status/")
+        result = requests.get("http://0.0.0.0/reservations/status/")
         result = json.loads(result.content)
         status:json = {"apiversion": "1.3.0", "authors": ["Jakob Vollmer", "Louis Müller"]}
         self.assertEqual(result, status)
